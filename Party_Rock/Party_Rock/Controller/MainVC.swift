606f416afba1f0c3aa1665460dc1d886ed4857bb
Nikolas Ponomarov <beardings@users.noreply.github.com>
2017-08-30 23:00:08 +0300

    custom fonts, create func what downloading image from the internet

--- Party_Rock/Party_Rock/Controller/MainVC.swift
+++ Party_Rock/Party_Rock/Controller/MainVC.swift
@@ -17,9 +17,15 @@ class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let p1 = PartyRock(imageURL: "http://www.forestalpark.com/madrid/wp-content/uploads/Logo-aventura-tirolinas-forestal-park-mobile.jpg", videoURL: "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/jgo1MfHj79w?ecver=1\" frameborder=\"0\" allowfullscreen></iframe>", videoTitle: "Extreme - He Man Woman Hater (cover)")
-    
+        let p1 = PartyRock(imageURL: "https://images.genius.com/dabe8f4ab86947d358c5e7a3e2a5de30.1000x1000x1.jpg", videoURL: "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/jgo1MfHj79w?ecver=1\" frameborder=\"0\" allowfullscreen></iframe>", videoTitle: "He man woman hater")
+        let p2 = PartyRock(imageURL: "https://images.genius.com/dabe8f4ab86947d358c5e7a3e2a5de30.1000x1000x1.jpg", videoURL: "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/jgo1MfHj79w?ecver=1\" frameborder=\"0\" allowfullscreen></iframe>", videoTitle: "He man woman hater")
+        let p3 = PartyRock(imageURL: "https://images.genius.com/dabe8f4ab86947d358c5e7a3e2a5de30.1000x1000x1.jpg", videoURL: "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/jgo1MfHj79w?ecver=1\" frameborder=\"0\" allowfullscreen></iframe>", videoTitle: "He man woman hater")
+        let p4 = PartyRock(imageURL: "https://images.genius.com/dabe8f4ab86947d358c5e7a3e2a5de30.1000x1000x1.jpg", videoURL: "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/jgo1MfHj79w?ecver=1\" frameborder=\"0\" allowfullscreen></iframe>", videoTitle: "He man woman hater")
+        
         partyRocks.append(p1)
+        partyRocks.append(p2)
+        partyRocks.append(p3)
+        partyRocks.append(p4)
         
         tableView.delegate = self
         tableView.dataSource = self
