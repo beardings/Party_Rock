e679cbe26d438b524bb58e1b418feb922f557b6c
Nikolas Ponomarov <beardings@users.noreply.github.com>
2017-08-30 14:34:09 +0300

    add UITableView + NewViwControler for vieo when song selected

---
+++ Party_Rock/Party_Rock/Controller/MainVC.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Party_Rock
+//
+//  Created by Nikolas Ponomarov on 30.08.17.
+//  Copyright © 2017 Nikolas Ponomarov. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
